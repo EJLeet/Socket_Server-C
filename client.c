a3381b6ad41e5ee25d1a9ea8c9a64c83762ecaba
Ethan Leet <ethanjleet@gmail.com>
2021-09-05 23:59:51 +0000

    initial socket setup info

--- client.c
+++ client.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+#include <netinet/in.h>
+#define BUF_SIZE 1024
 
 int main(int argc, char* argv[])
 {// read in command line arguements as game parameters
-    char *game_type = argv[1], *server_name = argv[2];
-    int port_number = atoi(argv[3]);
+    char *game_type = argv[1], /**server_name = argv[2],*/ client_buf[BUF_SIZE], server_reply[BUF_SIZE];
+    int /*port_number = atoi(argv[3]),*/ clientsock, res;
+    struct sockaddr_in server;
 
-    printf("%s %s %d\n", game_type, server_name, port_number);
+    // take computer as arg, convert to ip, pass ip below
+
+    // assign server address and port
+    server.sin_addr.s_addr = inet_addr(argv[2]);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(atoi(argv[3]));
+
+    // create socket
+    clientsock = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientsock < 0) perror("Could not create socket");
+    printf("Socket created\n");
+
+    // connect to remote server
+    res = connect(clientsock, (struct sockaddr *) &server, sizeof(server));
+    if (res == -1)
+    {
+        printf("Connection failed\n");
+        exit(1);
+    }
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,10 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BACKLOG 10
 
 int main(int argc, char* argv[])
 {// read in command line arguements as game parameters
-    int port_number = atoi(argv[1]), game_args = atoi(argv[2]);
+    int /*port_number = atoi(argv[1]),*/ game_args = atoi(argv[2]), serversock, clientsock, res;
     char *game_type = argv[3];
+    struct sockaddr_in server, client;
+
+    // Create TCP socket
+    serversock = socket(AF_INET, SOCK_STREAM, 0);
+    if(serversock == -1)
+    {
+        printf("Creating socket failed\n");
+        exit(1);
+    }
+    printf("Socket successfully created\n");
+
+    // Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(atoi(argv[1]));
+
+    // Bind addr to socket
+    res = bind(serversock, (struct sockaddr *) &server, sizeof(server));
+    if(res < 0)
+    {
+        printf("Bind failed\n");
+        exit(1);
+    }
+    printf("Bind was successfully completed\n");
+
+    // Listen
+    res = listen(serversock, BACKLOG);
+    if(res != 0)
+    {
+        printf("Listen failed\n");
+        exit(1);
+    }
+    printf("Waiting for incoming connections...\n");
+
+    // Accept connection from client
+    int clientlen = sizeof(client);
+    clientsock = accept(serversock, (struct sockaddr *) &client, &clientlen);
+    if(clientsock < 0)
+    {
+        perror("Accept failed");
+        exit(1);
+    }
+    else printf("Connection accepted\n");
 
     return 0;
 }
