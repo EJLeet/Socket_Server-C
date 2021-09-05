a4865c9b4b26136b29e62219063e7433a02659fa
Ethan Leet <ethanjleet@gmail.com>
2021-09-05 11:32:11 +0000

    added basic info

--- client.c
+++ client.c
@@ -1,4 +1,12 @@
-int main(void)
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{// read in command line arguements as game parameters
+    char *game_type = argv[1], *server_name = argv[2];
+    int port_number = atoi(argv[3]);
+
+    printf("%s %s %d\n", game_type, server_name, port_number);
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,4 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char* argv[])
-{
+{// read in command line arguements as game parameters
+    int port_number = atoi(argv[1]), game_args = atoi(argv[2]);
+    char *game_type = argv[3];
+
     return 0;
 }
