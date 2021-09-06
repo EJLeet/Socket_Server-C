9fbf2792e596d9187e5613f47c27dd9c1fc97162
Ethan Leet <ethanjleet@gmail.com>
2021-09-06 01:07:12 +0000

    resolve hostname

--- client.c
+++ client.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/types.h>
-//#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
 #define BUF_SIZE 1024
+
+void checkHostEntry(struct hostent * hostentry);
 
 int main(int argc, char* argv[])
 {// read in command line arguements as game parameters
-    char *game_type = argv[1], /**server_name = argv[2],*/ client_buf[BUF_SIZE], server_reply[BUF_SIZE];
-    int /*port_number = atoi(argv[3]),*/ clientsock, res;
+    char *game_type = argv[1], client_buf[BUF_SIZE], server_reply[BUF_SIZE], *ip_buffer;
+    int clientsock, res;
     struct sockaddr_in server;
-
+    struct hostent *host;
+    
     // take computer as arg, convert to ip, pass ip below
-
+    host = gethostbyname(argv[2]);
+    checkHostEntry(host);
+    ip_buffer = inet_ntoa(*((struct in_addr*)host->h_addr_list[0]));
+  
     // assign server address and port
-    server.sin_addr.s_addr = inet_addr(argv[2]);
+    server.sin_addr.s_addr = inet_addr(ip_buffer);
     server.sin_family = AF_INET;
     server.sin_port = htons(atoi(argv[3]));
 
@@ -32,4 +42,13 @@ int main(int argc, char* argv[])
     }
 
     return 0;
+}
+
+void checkHostEntry(struct hostent * hostentry)
+{
+    if (hostentry == NULL)
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
 }
