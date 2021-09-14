154f9ae431402f80dfb4e53a77e0dd31cb8fb537
Ethan Leet <ethanjleet@gmail.com>
2021-09-14 14:12:25 +0000

    comments added to dependencies

--- dependencies.h
+++ dependencies.h
@@ -13,7 +13,14 @@
 #define BUF_SIZE 1024
 
 int server_setup(int portnum, int backlog)
-{
+{/*
+                Handles server setup:
+                create sockes
+                setup server
+                bind
+                listen
+                returns server socket
+                                                    */
     struct sockaddr_in server; 
     int sock, res;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -41,7 +48,12 @@ int server_setup(int portnum, int backlog)
 }
 
 int accept_clients(int serversock)
-{
+{/*
+                Handles accepting clients:
+                accept clientd
+                sends them welcome message
+                returns client id
+                                                    */
     int client_id;
     struct sockaddr_in client;
     char welcome[] = "Welcome to the game";
@@ -68,31 +80,33 @@ void get_host_ip(struct hostent * hostentry)
 }
 
 int client_setup(char *host_name, int portnum) 
-{
+{/*
+                Handles client connection to server:
+                gets hostname and converts to ip
+                assign server address and port
+                create socket
+                connect to socket
+                returns client id
+                                                    */
     struct sockaddr_in server;
     struct hostent *host;
     char *ip_buffer, server_reply[BUF_SIZE];
     int res, client_id;
-
-        // take computer as arg, convert to ip, pass ip to sockadd_in struct
+    // take computer as arg, convert to ip, pass ip to sockadd_in struct
     host = gethostbyname(host_name);
     get_host_ip(host);
     ip_buffer = inet_ntoa(*((struct in_addr*)host->h_addr_list[0]));
-  
     // assign server address and port
     server.sin_addr.s_addr = inet_addr(ip_buffer);
     server.sin_family = AF_INET;
     server.sin_port = htons(portnum);
-
     // create socket
     if ((client_id = socket(AF_INET, SOCK_STREAM, 0)) < 0) perror("ERROR! Could not create socket");
-
     if ((res = connect(client_id, (struct sockaddr *) &server, sizeof(server))) == -1)
     {// connect to remote server
         printf("Connection failed\n");
         exit(1);
     }
-
     recv(client_id, server_reply, BUF_SIZE, 0); // receive welcome message
     printf("%s\n", server_reply); // print welcome message
     memset(server_reply, '\0', sizeof(server_reply)); // clear server reply buffer for next message
